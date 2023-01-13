856b0eaa72e7b5347b1e5ec17db40f35d41e62fa
Patrick Kage <patrick.r.kage@gmail.com>
2023-01-12 22:50:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Poetry Jupyter plugin
+
+## overview
+
+This is a really simple plugin to allow you to install your
+[Poetry](https://python-poetry.org) virtual environment as a
+[Jupyter](https://jupyter.org) kernel. You may wish to do this to keep your
+dependencies locked down for reproducible notebooks, or to set up a single
+"data science" notebook for one-off calculations without fiddling about with
+installing packages globally or dealing with `ipykernel` directly.
+
+## getting started
+
+Install the plugin with:
+
+```
+$ poetry self add poetry-jupyter-plugin
+```
+
+Then, from within your poetry project:
+
+```
+$ poetry install ipykernel -G dev
+$ poetry jupyter install
+```
+
+Remove the kernelspec with:
+
+```
+$ poetry jupyter remove
+```
+
+### configuration
+
+By default, the installed kernel will use the name of the project and a default
+Poetry icon. To configure these options, add these lines to your `pyproject.toml`:
+
+```
+[tool.jupyter.kernel]
+name = "my-cool-kernel"
+display = "My cool kernel"
+icon = "/path/to/icon.png"
+```
+
+## prior art
+
+There are other projects in this space, notably Pathbird's [`poetry-kernel`].
+`poetry-kernel` installs a single kernelspec globally which then patches the
+virtualenv based on the specific project folder that you're running Jupyter in.
+This has some pros and cons over this project.
+
+Pros:
+
+1. Single kernelspec, avoiding polluting the kernelspec list with lots of specs.
+2. Easy context switching between projects.
+
+Cons:
+
+1. Notebooks have to be in the same folder (or a subfolder from) as the
+   `pyproject.toml` folder.
+2. Requires forwarding signals from the launcher into Jupyter, introducing a
+   layer of complexity and is brittle to changes in Jupyter protocol/underlying
+   OS.
+3. Implicit dependency on `ipykernel`, and may fail to start without it.
+
+In contrast, this project installs one kernelspec per virtualenv and leaves it
+up to Jupyter to launch the kernel normally without interception. This design
+decision also allows multiple projects to be based out of one kernel.
+Additionally, the tool checks for the existence of `ipykernel` to make sure
+that the kernel can be installed properly.
+
+## who?
+
+This was written by [patrick kage](//ka.ge).
---
+++ poetry_jupyter/plugin.py
@@ -0,0 +1,246 @@
+from functools import reduce
+from pprint import pprint
+import tempfile
+import os
+import json
+import platform
+
+import asset
+from cleo.commands.command import Command
+from poetry.plugins.application_plugin import ApplicationPlugin
+from poetry.utils.env import EnvManager, VirtualEnv
+from jupyter_client.kernelspec import KernelSpecManager
+
+def optional_chain(root, keys):
+    try:
+        return reduce(lambda a,c: a.get(c), keys.split('.'), root)
+    except AttributeError:
+        return None
+
+
+class JupyterCommand(Command):
+    name = 'base jupyter command'
+    description = '(internal) jupyter base methods'
+
+
+    def get_requested_kernel_opts(self):
+        # load the configuration
+        data = self.application.poetry.pyproject.data
+
+        # get the current name or default to project name
+        name = optional_chain(data, 'tool.jupyter.kernel.name')
+        if name is None:
+            name = optional_chain(data, 'tool.poetry.name')
+
+        # get the current display name or default to project name
+        display_name = optional_chain(data, 'tool.jupyter.kernel.display')
+        if display_name is None:
+            display_name = optional_chain(data, 'tool.poetry.name')
+
+        # get the current display name or default to project name
+        icon = optional_chain(data, 'tool.jupyter.kernel.icon') # None is an ok default
+
+        # create kernel object
+        return {
+            'icon': icon,
+            'name': name,
+            'display': display_name
+        }
+
+
+    def get_venv(self):
+        manager = EnvManager(self.application.poetry)
+
+        if len(manager.list()) == 0:
+            return None
+
+        return manager.list()[0]
+
+
+    def has_ipykernel(self):
+        venv = self.get_venv()
+        if venv is None:
+            return False
+        
+        ipykernels = venv.site_packages.find('ipykernel')
+
+        if len(ipykernels) == 0:
+            return False
+
+        return True
+
+
+    def install_venv(self):
+        # print(f'virtualenv python: {venv.python}')
+        return False
+
+
+    def get_all_installed_kernels(self):
+        ksm = KernelSpecManager()
+        return ksm.get_all_specs()
+        
+
+    def get_kernelspec_for_project(self):
+        kernels = self.get_all_installed_kernels()
+
+        venv = self.get_venv()
+        if venv is None:
+            return None
+
+        for key, obj in kernels.items():
+            argv = optional_chain(obj, 'spec.argv')
+            if argv is None:
+                continue
+
+            if argv[0] == venv.python:
+                obj['key'] = key
+                return obj
+
+        return None
+
+    def get_preferred_spec_prefix(self):
+        # fixes on macOS
+        if platform.system() == 'Darwin':
+            manager = KernelSpecManager()
+            for dir in list(manager.kernel_dirs):
+
+                if dir.endswith('/Library/Jupyter/kernels'):
+                    return dir
+
+        return None
+
+class JupyterShowCommand(JupyterCommand):
+    name = 'jupyter show'
+    description = 'Show Jupyter kernels in this project.'
+
+    def handle(self) -> int:
+        venv = self.get_venv()
+        if venv is None:
+            self.line('<info>No virtualenv has been created for this project yet.</>')
+            return 1
+        
+        spec = self.get_kernelspec_for_project()
+
+        if spec is None:
+            kernel = self.get_requested_kernel_opts()
+            # self.line('<info>This kernel is not installed yet.</>')
+            self.line(f'Kernel <c1>{kernel["name"]}</> has not yet been installed.')
+            return 1
+
+
+        self.line(f"<c1>{spec['spec']['display_name']}</> ({spec['resource_dir']})")
+
+        return 0
+
+
+class JupyterEnableCommand(JupyterCommand):
+
+    name = 'jupyter install'
+    description = 'Register this project as a Jupyter kernel.'
+
+    def handle(self) -> int:
+
+        # check if it's already installed
+        spec = self.get_kernelspec_for_project()
+        if spec is not None:
+            self.line(f'Kernel <c1>{spec["key"]}</> is already installed!')
+            return 1
+
+
+        # check if pkgs are available
+        if not self.has_ipykernel():
+            self.line('<error>ipykernel is not installed in this project!</>')
+            self.line('You can install it with <c2>poetry add ipykernel -G dev</>')
+            return 1
+
+        # get requested config details
+        kernel = self.get_requested_kernel_opts()
+
+        # get icon
+        if kernel['icon'] is not None and not os.path.exists(kernel['icon']):
+            self.line(f'<error>Requested icon {kernel["icon"]} does not exist!')
+            return 1
+
+
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            spec = {
+                 'argv': [
+                     self.get_venv().python,
+                     '-m',
+                     'ipykernel_launcher',
+                     '-f',
+                     '{connection_file}'
+                 ],
+                 'display_name': kernel['display'],
+                 'env': {},
+                 'interrupt_mode': 'signal',
+                 'language': 'python',
+                 'metadata': {'debugger': True}
+            }
+
+            # asset lib only gets you bytes, so we'll do it like this
+            if kernel['icon'] is None:
+                icon_data = asset.load('poetry_jupyter:assets/poetry.png').read()
+            else:
+                icon_data = open(kernel['icon'], 'rb').read()
+
+            with open(os.path.join(tmpdir, 'logo-32x32.png'), 'wb') as fp:
+                fp.write(icon_data)
+
+            with open(os.path.join(tmpdir, 'logo-64x64.png'), 'wb') as fp:
+                fp.write(icon_data)
+
+            with open(os.path.join(tmpdir, 'kernel.json'), 'w') as fp:
+                json.dump(spec, fp, indent=4)
+
+            manager = KernelSpecManager()
+            manager.install_kernel_spec(
+                tmpdir,
+                kernel_name=kernel['name'],
+                user=True
+            )
+
+        self.line(f'Kernel <c1>{kernel["name"]}</> has been installed!')
+
+        return 0
+
+
+class JupyterDisableCommand(JupyterCommand):
+
+    name = 'jupyter remove'
+    description = 'Remove this project\'s Jupyter kernel.'
+
+    def handle(self):
+        spec = self.get_kernelspec_for_project()
+        kernel = self.get_requested_kernel_opts()
+        if spec is None:
+            self.line(f'Kernel <c1>{kernel["name"]}</> is not installed!')
+            return 1
+
+        if not self.confirm(f'Really remove kernel <c2>{kernel["name"]}</c2>?', False):
+            return 0
+
+        manager = KernelSpecManager()
+
+        manager.remove_kernel_spec(kernel['name'])
+
+        self.line(f'\nSuccessfully removed kernel <c1>{kernel["name"]}</>.')
+
+        return 0
+
+
+class JupyterKernelPlugin(ApplicationPlugin):
+    def activate(self, application):
+        commands = [
+            ('jupyter show', lambda: JupyterShowCommand()),
+            ('jupyter install', lambda: JupyterEnableCommand()),
+            ('jupyter remove', lambda: JupyterDisableCommand())
+        ]
+
+    
+        for command, factory in commands:
+            application.command_loader.register_factory(
+                command,
+                factory
+            )
